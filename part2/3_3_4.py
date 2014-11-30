25cce995a99197113c0e5cd8952a4e65d5d13ede
kouhei0725 <feel-the-wind.sakura@hotmail.co.jp>
2014-11-30 21:43:22 +0900

    mathモジュールの使い方

---
+++ part2/3_3_4.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import math
+
+# π
+print math.pi
+
+# e
+print math.e
+
+# cosin
+print math.cos(math.pi)
+
+# log
+print math.log(math.e)
+
+# 階乗
+print math.factorial(3)
+
+# 平方根を返す
+print math.sqrt(100)
+
+
