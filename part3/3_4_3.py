51a4c9bd16264e002bc3471542e606615d8aea50
kouhei0725 <feel-the-wind.sakura@hotmail.co.jp>
2014-12-03 21:31:58 +0900

    リストの使い方１回目

---
+++ part3/3_4_3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+x = [1,2,3.0,"a","bbb"]
+
+# インデックシング
+print x[0]
+
+# インデックシング(後ろから)
+print x[-1]
+
+# リストのスライス
+print x[1:3]
+print x[1:]
+
+# イテレーション
+for item in x:
+    print(item)
+
