200d20c3050fe8d1923c4264459b331fb58c1217
kouhei0725 <feel-the-wind.sakura@hotmail.co.jp>
2014-12-16 23:09:45 +0900

    辞書の操作を追加

--- part3/3_6.py
+++ part3/3_6.py
@@ -19,4 +19,32 @@ print d.get('key4')
 print d.get('key10')
 
 
+# イテレーションアクセス
+for key in d:
+    print key
+
+
+for value in d.values():
+    print value
+
+for key,value in d.items():
+    print (key,value)
+
+# 辞書の更新
+d['key1'] = 'vala'
+print d['key1']
+
+# 辞書に追加
+d['key5'] = 'val5'
+
+for key,value in d.items():
+    print (key,value)
+
+
+# 辞書からキーを削除
+del d['key2']
+
+print "del key2"
+for key,value in d.items():
+    print (key,value)
 
