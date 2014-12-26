93b751922a7f5988c9d6e45feb9fb1df9b689142
kouhei <feel-the-wind.sakura@hotmail.co.jp>
2014-12-26 22:33:14 +0900

    for文の使い方を追加

--- part4/4_4.py
+++ part4/4_4.py
@@ -1,2 +1,13 @@
 for i in ["apple","orange","lemon"]:
     print(i)
+
+for i in range(5):
+    print(i)
+
+for index, name in enumerate(["apple","orange","lemon"]):
+   print(index, name)
+
+for i in range(3):
+   print(i)
+else:
+   print("done")
