5bf04108cf26c458f7437f1d214c7fc3196ecd03
kouhei <feel-the-wind.sakura@hotmail.co.jp>
2015-01-07 21:52:47 +0900

    途中

---
+++ part4/4_6_4.py
@@ -0,0 +1,10 @@
+def write(file_name, dict_input):
+    f = None
+    try:
+        f = open(file_name, 'w')
+        data = dict_input['data']
+        f.write(data)
+    except KeyError as e:
+        print('
+
+
