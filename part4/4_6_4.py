3d34319224f29721c9f6265b168369897a2a220f
kouhei <feel-the-wind.sakura@hotmail.co.jp>
2015-01-13 21:39:18 +0900

    エラーハンドリング

--- part4/4_6_4.py
+++ part4/4_6_4.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 def write(file_name, dict_input):
     f = None
     try:
@@ -5,11 +6,20 @@ def write(file_name, dict_input):
         data = dict_input['data']
         f.write(data)
     except KeyError as e:
-        print('
+        print('エラー種別: {0}'.format(type(e)))
+        print(e)
+        print ('キーが見つかりませんでした：{0}'.format(str(dict_input)))
+    except (FileNotFoundError, TypeError) as e:
+        print('エラー種別: {0}'.format(type(e)))
+        print(e)
+        print ('ファイルが開けませんでした：{0}'.format(file_name))
+    else:
+        print('問題なく処理ができました')
+    finally:
+        if f is not None:
+            print('ファイルを閉じます')
+            f.close()
 
-
-
-
-
-
+d = {'meta':'aaaaa', 'data':'bbbbbb'}
+write('test.txt',d)
 
